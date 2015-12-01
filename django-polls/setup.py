53c5cb78731bde143628e604609dc739578fb072
Jessica Langston <jessica.langston@rd.io>
2015-11-30 16:50:32 -0800

    Polls app now a separate Python package

---
+++ django-polls/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme: 
+	README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+	name='django-polls',
+	version='0.1',
+	packages=['polls'],
+	include_package_data=True,
+	license='BSD License', # example license
+	description='A simple Django app to conduct Web-based polls.',
+	long_description=README,
+	url='http://www.example.com/',
+	author='Jessica L.',
+	author_email='yourname@example.com',
+	classifiers=[
+		'Environment :: Web Environment',
+		'Framework :: Django',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: BSD License', # example license
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2.7.8',
+		'Topic :: Internet :: WWW/HTTP',
+		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+	],
+)
